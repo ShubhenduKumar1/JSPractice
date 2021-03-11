e09a3153127dd83fb9daf1905c9b842f594fcecb
Monu <shubhendukumarlal@gmail.com>
2021-03-11 14:08:05 +0530

    Added initial files

---
+++ quizz.js
@@ -0,0 +1,9 @@
+{/* <div class="start-btn"><button>Start Quiz</button></div> */}
+{/* <div class="info-box"></div> */}
+{/* <button class="quit">Exit Quiz</button>
+<button class="continue">Continue</button> */}
+const start_btn=document.querySelector('.start-btn');
+const info_div=document.querySelector('.info-box');
+const quit_btn=document.querySelector('.quit');
+const continue_btn=document.querySelector('.continue');
+
